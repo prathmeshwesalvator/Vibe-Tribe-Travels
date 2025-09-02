3e7ffaebbb62eda4f892973922be22accfd7b98d
prathmeshmore655 <prathmeshmore655@gmail.com>
2025-09-02 21:42:12 +0530

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // ✅ ensures correct asset paths on Vercel
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     cors: true,
     allowedHosts: [
-      '.ngrok-free.app', // ✅ allow all ngrok subdomains
+      '.ngrok-free.app',
     ],
   },
 })
